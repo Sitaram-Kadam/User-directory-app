9795fb3b767e40883271dfd469f1ad81187674c1
Sitaram-Kadam <kadamsitu1998@gmail.com>
2024-02-01 00:05:28 +0530

    This is first commit for user directory app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import PostContainer from "./PostContainer";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((dataNew) => setData(dataNew));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="dir-container">
+        <h2 className="dir">Directory</h2>
+        {data &&
+          data.map((user) => (
+            <div>
+              <button className="dir-user-btn"
+                onClick={() => setSelectedUser(user)}
+              >
+                <label className="d-name-label">
+                  Name:
+                  <p style={{ marginTop: "0px" }} key={user.id}>
+                    <span>{user.name}</span>
+                  </p>
+                </label>
+              </button>
+            </div>
+          ))}
+      </div>
+      <PostContainer user={selectedUser} />
     </div>
   );
 }
