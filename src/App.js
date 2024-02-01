f85a6380fdb9c2635084892bf7a0a9a38cd84f94
Sitaram-Kadam <157870541+Sitaram-Kadam@users.noreply.github.com>
2024-02-01 12:32:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,32 +8,46 @@ function App() {
   const [selectedUser, setSelectedUser] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((dataNew) => setData(dataNew));
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
   return (
-    <div >
-      <div className="dir-container">
-        <h2 className="dir">Directory</h2>
-        {data &&
-          data.map((user) => (
-            <div>
-              <button className="dir-user-btn"
-                onClick={() => setSelectedUser(user)}
-              >
-                <label className="d-name-label">
-                  Name:
-                  <p style={{ marginTop: "0px" }} key={user.id}>
-                    <span>{user.name}</span>
-                  </p>
-                </label>
-              </button>
-            </div>
-          ))}
+    <div>
+      {!data && <p>Loading...</p>}
+      <div>
+        {data && (
+          <div className="dir-container">
+            <h2 className="dir">Directory</h2>
+
+            {data.map((user) => (
+              <div>
+                <button
+                  className="dir-user-btn"
+                  onClick={() => setSelectedUser(user)}
+                >
+                  <label className="d-name-label">
+                    Name:
+                    <p style={{ marginTop: "0px" }} key={user.id}>
+                      <span>{user.name}</span>
+                    </p>
+                  </label>
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+        <PostContainer user={selectedUser} />
       </div>
-      <PostContainer user={selectedUser} />
     </div>
   );
 }
